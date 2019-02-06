346f5c0721301c8cc5dc8cbe91ab2020cc39fab4
Ivan Spiridonov <ivan.spiridonov@nhqsa.nato.int>
2019-02-06 14:41:27 +0100

    changes

--- Powershell/Various/Get-SystemReport/Get-SystemReport.ps1
+++ Powershell/Various/Get-SystemReport/Get-SystemReport.ps1
@@ -1,6 +1,42 @@
-#Get HTML system report for single computer
-#Requires -Version 3.0
+<#PSScriptInfo
 
+.VERSION 0.8.0
+
+.GUID f956f543-ef88-4df1-bbc6-52ab19df984c
+
+.AUTHOR Ivan Spiridonov
+
+.COMPANYNAME ATCO Frontec Europe
+
+.COPYRIGHT Feel Free to use it
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+It's more proof of concept than anything else
+
+.PRIVATEDATA 
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Creates HTML system info report for a single computer. CSS file needed is named ps.css(must reside in the same folder). 
+
+#> 
 [cmdletbinding()]
 Param(
     [string]$ComputerName,
@@ -13,14 +49,15 @@ Param(
 )
 
 Function Get-SystemInfo {
-
     if ($ComputerName) { 
         Write-Host "Starting Report on: $ComputerName"
      
         #this function has no real error handling
-        $cs = Get-WmiObject -ClassName Win32_computersystem -ComputerName $ComputerName  
+        $cs = Get-WmiObject -ClassName Win32_computersystem -ComputerName $ComputerName
+        $bios = Get-WmiObject -ClassName Win32_bios -ComputerName $ComputerName
         #this assumes a single processor
-        $proc = Get-WmiObject -ClassName win32_processor -ComputerName $ComputerName 
+        $proc = Get-WmiObject -ClassName win32_processor -ComputerName $ComputerName
+        #$hddSerial = Get-WmiObject -ClassName win32_physicalMedia | Select-Object -expandproperty SerialNumber
         $disklist = @(Get-WmiObject win32_logicaldisk | Where-Object {$_.drivetype -eq '3'} | `
                 Select-Object @{N = 'DriveLetter'; E = {$_.caption}}, `
                 description, `
@@ -28,6 +65,7 @@ Function Get-SystemInfo {
                 volumename, `
             @{N = 'SizeGB'; E = {[math]::Round(($_.size / 1GB), 2)}}, @{N = 'FreeSpaceGB'; E = {"{0:N2}" -f ($_.FreeSpace / 1GB)}}, `
             @{N = 'PercentFree'; E = {"$([Math]::round((($_.FreeSpace/$_.size) * 100)))%"}} 
+			
         ) 
     
 
@@ -42,6 +80,7 @@ Function Get-SystemInfo {
             MaxClock             = $proc.MaxClockSpeed
             L2size               = $proc.L2CacheSize
             L3Size               = $proc.L3CacheSize
+            SerialNumber         = $bios.SerialNumber
 
         }
    
@@ -49,13 +88,15 @@ Function Get-SystemInfo {
 
         Foreach ($disk in $disklist) {
             [PSCustomObject] @{
-                DriveLetter = $disk.DriveLetter
-                Description = $disk.Description
+                DriveLetter  = $disk.DriveLetter
+                Description  = $disk.Description
                 #Drivetype = $disk.Drivetype
-                Volumename  = $disk.Volumename
-                SizeGB      = $disk.'SizeGB'
-                FreeSpaceGB = $disk.FreeSpaceGB
-                PercentFree = $disk.PercentFree
+                Volumename   = $disk.Volumename
+                SizeGB       = $disk.'SizeGB'
+                FreeSpaceGB  = $disk.FreeSpaceGB
+                PercentFree  = $disk.PercentFree
+                Model        = Get-WmiObject -ClassName Win32_DiskDrive -computername $ComputerName |  Select-Object -ExpandProperty Model
+                SerialNumber = Get-WmiObject -ClassName win32_physicalMedia -computername $ComputerName | Select-Object -expandproperty SerialNumber
             }
         }
         #if ends here    
@@ -76,9 +117,9 @@ $fragments += $ImageTag
 $fragments += "<br><br>"
 $fragments += "<H2>OS Info</H2>"
 $fragments += Get-WmiObject -ClassName win32_operatingsystem -ComputerName $ComputerName |
-    Select-Object  pscomputername, @{Name = "Operating System"; Expression = {$_.Caption}}, Version, @{Name = "Install Date"; Expression = {$_.ConverttoDateTime($_.InstallDate)} } |
+    Select-Object  pscomputername, @{Name = "Operating System"; Expression = {$_.Caption}}, @{Name = "Model"; Expression = {"$($cs.Model)"}}, Version, @{Name = "Install Date"; Expression = {$_.ConverttoDateTime($_.InstallDate)} } |
     ConvertTo-Html -Fragment -As List
 $fragments += "<H2>System Info</H2>"
 $fragments += Get-SystemInfo -ComputerName $ComputerName | ConvertTo-Html -Fragment -As List
-$filename = "HtmlReport" + (Get-Date -Format d)
-ConvertTo-HTML -Title "OS Info" -body "$fragments" -PostContent "<H5><i>$((Get-Date -Format f))</i></H5>" -CSSUri "ps.css" | Set-Content "$filename.html"
+$filename = "HtmlReport" + "($ComputerName)"
+ConvertTo-HTML -Title "OS Info" -body "$fragments" -PostContent "<H5><i>Generated on: $((Get-Date -Format f))</i></H5>" -CSSUri "ps.css" | Set-Content "$filename.html"
