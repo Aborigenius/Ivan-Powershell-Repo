2cfb9740f37a88c45be7c86211c8dd069f2df0d6
Ivan Spiridonov <ivan.spiridonov@nhqsa.nato.int>
2019-02-08 15:50:47 +0100

    ps system info updated

--- Powershell/Various/Get-SystemReport/Get-SystemReport.ps1
+++ Powershell/Various/Get-SystemReport/Get-SystemReport.ps1
@@ -1,125 +1,126 @@
-<#PSScriptInfo
+#Requires -Version 3
+<#
+.SYNOPSIS
+  Creates html Report for remote computer/computers
 
-.VERSION 0.8.0
+.DESCRIPTION
+  Creates html Report for remote computer/computers
 
-.GUID f956f543-ef88-4df1-bbc6-52ab19df984c
+.PARAMETER <Parameter_Name>
+    ComputerName
 
-.AUTHOR Ivan Spiridonov
+.INPUTS
+  ComputerName, one or many seperated by ,
+  Accepts variable containg computer names as well
 
-.COMPANYNAME ATCO Frontec Europe
+.OUTPUTS
+  HTML file in the follwoing Format
+  COMPUTERNameHTMLReport<TodaysDate>.html
 
-.COPYRIGHT Feel Free to use it
-
-.TAGS 
-
-.LICENSEURI 
-
-.PROJECTURI 
-
-.ICONURI 
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS 
-
-.EXTERNALSCRIPTDEPENDENCIES 
-
-.RELEASENOTES
-It's more proof of concept than anything else
-
-.PRIVATEDATA 
-
+.NOTES
+  Version:        1.0
+  Author:         Ivan Spiridonov
+  Creation Date:  08-Feb-2019
+  Purpose/Change: Initial script development
+  Not Working for the local workstation (yet)
+  
+.EXAMPLE
+  Get-info.ps1 -ComputerName pc1,pc,pc3
 #>
-
-<# 
-
-.DESCRIPTION 
- Creates HTML system info report for a single computer. CSS file needed is named ps.css(must reside in the same folder). 
-
-#> 
 [cmdletbinding()]
 Param(
-    [string]$ComputerName,
-    $Name,
-    $Value,
-    [ValidateNotNull()]
-    [System.Management.Automation.PSCredential]
-    [System.Management.Automation.Credential()]
-    $Credential = [System.Management.Automation.PSCredential]::Empty
+    [string[]]$ComputerName
+    # $Name,
+    # $Value,
+    # [ValidateNotNull()]
+    # [System.Management.Automation.PSCredential]
+    # [System.Management.Automation.Credential()]
+    # $Credential = [System.Management.Automation.PSCredential]::Empty
 )
-
 Function Get-SystemInfo {
-    if ($ComputerName) { 
-        Write-Host "Starting Report on: $ComputerName"
+
+    Write-Host "Starting Report on: $pc"
      
-        #this function has no real error handling
-        $cs = Get-WmiObject -ClassName Win32_computersystem -ComputerName $ComputerName
-        $bios = Get-WmiObject -ClassName Win32_bios -ComputerName $ComputerName
-        #this assumes a single processor
-        $proc = Get-WmiObject -ClassName win32_processor -ComputerName $ComputerName
-        #$hddSerial = Get-WmiObject -ClassName win32_physicalMedia | Select-Object -expandproperty SerialNumber
-        $disklist = @(Get-WmiObject win32_logicaldisk | Where-Object {$_.drivetype -eq '3'} | `
-                Select-Object @{N = 'DriveLetter'; E = {$_.caption}}, `
-                description, `
-                drivetype, `
-                volumename, `
-            @{N = 'SizeGB'; E = {[math]::Round(($_.size / 1GB), 2)}}, @{N = 'FreeSpaceGB'; E = {"{0:N2}" -f ($_.FreeSpace / 1GB)}}, `
-            @{N = 'PercentFree'; E = {"$([Math]::round((($_.FreeSpace/$_.size) * 100)))%"}} 
-			
-        ) 
-    
+    #this function has no real error handling
+    $cs = Get-CimInstance -ClassName Win32_computersystem -ComputerName $pc  
+    #this assumes a single processor
+    $proc = Get-CimInstance -ClassName win32_processor -ComputerName $pc 
+    #Get-CimInstance -ClassName Win32_physicalMedia | Select-Object Tag, SerialNumber
+    $data = [ordered]@{
+        TotalPhysicalMemGB   = $cs.TotalPhysicalMemory / 1GB -as [int]
+        NumProcessors        = $cs.NumberOfProcessors
+        NumLogicalProcessors = $cs.NumberOfLogicalProcessors
+        HyperVisorPresent    = $cs.HypervisorPresent
+        DeviceID             = $proc.DeviceID
+        Name                 = $proc.Name
+        MaxClock             = $proc.MaxClockSpeed
+        L2size               = $proc.L2CacheSize
+        L3Size               = $proc.L3CacheSize
+  
+    }
 
-    
-        $data = [ordered]@{
-            TotalPhysicalMemGB   = $cs.TotalPhysicalMemory / 1GB -as [int]
-            NumProcessors        = $cs.NumberOfProcessors
-            NumLogicalProcessors = $cs.NumberOfLogicalProcessors
-            HyperVisorPresent    = $cs.HypervisorPresent
-            DeviceID             = $proc.DeviceID
-            Name                 = $proc.Name
-            MaxClock             = $proc.MaxClockSpeed
-            L2size               = $proc.L2CacheSize
-            L3Size               = $proc.L3CacheSize
-            SerialNumber         = $bios.SerialNumber
-
-        }
-   
-        New-Object -TypeName PSObject -Property $data
-
-        Foreach ($disk in $disklist) {
-            [PSCustomObject] @{
-                DriveLetter  = $disk.DriveLetter
-                Description  = $disk.Description
-                #Drivetype = $disk.Drivetype
-                Volumename   = $disk.Volumename
-                SizeGB       = $disk.'SizeGB'
-                FreeSpaceGB  = $disk.FreeSpaceGB
-                PercentFree  = $disk.PercentFree
-                Model        = Get-WmiObject -ClassName Win32_DiskDrive -computername $ComputerName |  Select-Object -ExpandProperty Model
-                SerialNumber = Get-WmiObject -ClassName win32_physicalMedia -computername $ComputerName | Select-Object -expandproperty SerialNumber
+    Invoke-Command -ComputerName $pc -ScriptBlock { Get-CimInstance Win32_DiskDrive | ForEach-Object {
+            $disk = $_
+            $partitions = "ASSOCIATORS OF " +
+            "{Win32_DiskDrive.DeviceID='$($disk.DeviceID)'} " +
+            "WHERE AssocClass = Win32_DiskDriveToDiskPartition"
+            Get-CimInstance -Query $partitions  | ForEach-Object {
+                $partition = $_
+                $drives = "ASSOCIATORS OF " +
+                "{Win32_DiskPartition.DeviceID='$($partition.DeviceID)'} " +
+                "WHERE AssocClass = Win32_LogicalDiskToPartition"
+                Get-CimInstance -Query $drives | ForEach-Object {
+                    New-Object -Type PSCustomObject -Property @{
+                        Disk        = $disk.DeviceID
+                        #DiskSize    = $disk.Size
+                        DiskModel   = $disk.Model
+                        Partition   = $partition.Name
+                        RawSize     = $partition.Size
+                        DriveLetter = $_.DeviceID
+                        VolumeName  = $_.VolumeName
+                        Size        = [math]::Round(($_.Size / 1GB), 2) #'{0:d} GB' -f [int]($_.Size / 1GB)
+                        FreeSpace   = [math]::Round(($_.FreeSpace / 1GB), 2)
+                              
+                    }
+                }
             }
         }
-        #if ends here    
-    }
-    else {
-        Write-Host "Computer Name Not Passed on the Command Line"
-    } 
-}
 
-$fragments = @()
-$imagePath = ".\icons\006-gear.png"
-$imageBits = [Convert]::ToBase64String((Get-Content $ImagePath -Encoding Byte))
-$ImageFile = Get-Item $ImagePath
-$ImageType = $ImageFile.Extension.Substring(1) #strip off the leading .
-$ImageTag = "<Img src='data:image/$ImageType;base64,$($ImageBits)' Alt='$($ImageFile.Name)' style='float:left' width='60' height='60' hspace=10>"
-$fragments += $ImageTag
-#adjust spacing - takes trial and error
-$fragments += "<br><br>"
-$fragments += "<H2>OS Info</H2>"
-$fragments += Get-WmiObject -ClassName win32_operatingsystem -ComputerName $ComputerName |
-    Select-Object  pscomputername, @{Name = "Operating System"; Expression = {$_.Caption}}, @{Name = "Model"; Expression = {"$($cs.Model)"}}, Version, @{Name = "Install Date"; Expression = {$_.ConverttoDateTime($_.InstallDate)} } |
-    ConvertTo-Html -Fragment -As List
-$fragments += "<H2>System Info</H2>"
-$fragments += Get-SystemInfo -ComputerName $ComputerName | ConvertTo-Html -Fragment -As List
-$filename = "HtmlReport" + "($ComputerName)"
-ConvertTo-HTML -Title "OS Info" -body "$fragments" -PostContent "<H5><i>Generated on: $((Get-Date -Format f))</i></H5>" -CSSUri "ps.css" | Set-Content "$filename.html"
+    }
+    Get-CimInstance -ClassName Win32_physicalMedia -ComputerName $pc | ForEach-Object { $hd = $_
+        New-Object -TypeName PSCustomObject -Property @{
+            Disk    = $hd.Tag
+            Serial = $hd.SerialNumber
+        }
+    }
+    # Invoke-Command -ComputerName $pc -ScriptBlock {Get-CimInstance -ClassName Win32_physicalMedia | ForEach-Object { $hd = $_
+    #   New-Object -TypeName PSCustomObject -Property @{
+    #     Tag = $hd.Tag
+    #     Serial = $hd.SerialNumber
+    #   }
+    #   }}
+}
+foreach ($pc in $ComputerName) {
+    
+    if (Test-Connection -ComputerName $pc -Count 1 -Quiet -ErrorAction SilentlyContinue) {
+        # If Console Output is needed uncomment the next line
+        # Get-SystemInfo $pc
+        $fragments = @()
+        $imagePath = ".\icons\006-gear.png"
+        $imageBits = [Convert]::ToBase64String((Get-Content $ImagePath -Encoding Byte))
+        $ImageFile = Get-Item $ImagePath
+        $ImageType = $ImageFile.Extension.Substring(1) #strip off the leading .
+        $ImageTag = "<Img src='data:image/$ImageType;base64,$($ImageBits)' Alt='$($ImageFile.Name)' style='float:left' width='50' height='50' hspace=10>"
+        $fragments += $ImageTag
+        #adjust spacing - takes trial and error
+        $fragments += "<br><br>"
+        $fragments += "<H2>OS Info</H2>"
+        $fragments += Get-CimInstance -ClassName win32_operatingsystem -ComputerName $pc |
+            Select-Object  pscomputername, @{Name = "Operating System"; Expression = {$_.Caption}}, Version, @{Name = "Install Date"; Expression = {$_.ConverttoDateTime($_.InstallDate)} } |
+            ConvertTo-Html -Fragment -As List
+        $fragments += "<H2>System Info</H2>"
+        $fragments += Get-SystemInfo -ComputerName $pc | ConvertTo-Html -Fragment -As List
+        $filename = $pc.ToString().ToUpper() + "HtmlReport" + $((Get-Date).ToString('MM-dd-yyyy'))
+        ConvertTo-HTML -Title "OS Info" -body "$fragments" -PostContent "<H5><i>Generated on: $((Get-Date -Format f))</i></H5>" -CSSUri "ps.css" | Set-Content "$filename.html"
+    }
+}
--- Powershell/Various/Get-SystemReport/pstest.ps1
+++
@@ -1,3 +0,0 @@
-$disklist = @(Get-WmiObject win32_logicaldisk | Where-Object {$_.drivetype -eq '3'} | Select-Object @{N='DriveLetter';E={$_.caption}},description,drivetype,volumename,@{N='SizeGB';E={[math]::Round(($_.size/1GB),2)}},@{N='FreeSpaceGB';E={"{0:N2}" -f ($_.FreeSpace /1GB)}})
-
-@{N='FreeSpaceGB';E={"{0:N2}" -f ($_.FreeSpace /1GB)}}
